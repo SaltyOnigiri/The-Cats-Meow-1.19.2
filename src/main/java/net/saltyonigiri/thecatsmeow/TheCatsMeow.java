dc7c4ce2b22cde9d69e24478e0a492beb42ab778
Camryn Way <alecria61093@gmail.com>
2023-07-29 10:06:23 -0400

    Initial commit

---
+++ src/main/java/net/saltyonigiri/thecatsmeow/TheCatsMeow.java
@@ -0,0 +1,16 @@
+package net.saltyonigiri.thecatsmeow;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TheCatsMeow implements ModInitializer {
+	public static final String MOD_ID = "thecatsmeow";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/saltyonigiri/thecatsmeow/TheCatsMeowClient.java
@@ -0,0 +1,10 @@
+package net.saltyonigiri.thecatsmeow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheCatsMeowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
